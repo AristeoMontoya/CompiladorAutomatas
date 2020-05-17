68cae7522571bec3c9c246f1e57e4532c9215917
Aristeo Montoya <aristeomontoya98@gmail.com>
2020-05-17 14:59:07 -0600

    Construcción del árbol
    Se implementó el método que construye el árbol a partir de una expresión como arreglo de caracteres

--- src/main/Arbol.java
+++ src/main/Arbol.java
@@ -1,59 +1,66 @@
 package main;
 
+import java.util.Stack;
+
+class Nodo {
+	char valor;
+	Nodo izquierda, derecha;
+
+	Nodo(char valor) {
+		this.valor = valor;
+		izquierda = derecha = null;
+	}
+}
+
 public class Arbol {
-	private Nodo raiz;
 
 	public Arbol() {
-//		raiz = new Nodo();
 	}
 
-	public boolean vacio() {
-		return raiz == null;
+	private boolean esOperador(char c) {
+		return c == '+' || c == '-' || c == '/' || c == '*';
 	}
 
-	public void insertar(Token t) {
-		if (vacio()) {
-			Nodo nuevo = new Nodo();
-			nuevo.setDato(t);
-			nuevo.setHojaDerecha(new Arbol());
-			nuevo.setHojaIzquierda(new Arbol());
-			raiz = nuevo;
-		} else {
-			if (precedencia(t.getSimbolo()) > precedencia(raiz.getDato().getSimbolo())) {
-				raiz.getHojaDerecha().insertar(t);
+	public void infija(Nodo t) {
+		if (t != null) {
+			infija(t.izquierda);
+			System.out.println(t.valor + " ");
+			infija(t.derecha);
+		}
+	}
+
+	public double resolver(Nodo t) {
+		if(t != null) {
+			double valor1, valor2;
+			valor1 = resolver(t.izquierda);
+			valor2 = resolver(t.derecha);
+		}
+		return 0;
+	}
+
+	public Nodo construirArbol(char[] expresion) {
+		Stack<Nodo> pila = new Stack<>();
+		Nodo t1, t2, t3;
+
+		for (int i = 0; i < expresion.length; i++) {
+			char actual = expresion[i];
+			if (!esOperador(actual)) {
+				t1 = new Nodo(actual);
+				pila.push(t1);
 			} else {
-				raiz.getHojaIzquierda().insertar(t);
+				t1 = new Nodo(actual);
+
+				t2 = pila.pop();
+				t3 = pila.pop();
+
+				t1.derecha = t2;
+				t1.izquierda = t3;
+
+				pila.push(t1);
 			}
 		}
-	}
-
-	public void preOrden() {
-		if (!vacio()) {
-			System.out.println(raiz.getDato().getSimbolo() + " : " + raiz.getDato().getExpresion() + "\n");
-			raiz.getHojaIzquierda().preOrden();
-			raiz.getHojaDerecha().preOrden();
-		}
-	}
-
-	private int precedencia(String lexema) {
-		int precedencia = 0;
-		switch (lexema) {
-			case "=":
-				precedencia = 5;
-				break;
-			case "*":
-			case "/":
-				precedencia = 2;
-				break;
-			case "+":
-			case "-":
-				precedencia = 1;
-				break;
-			default:
-				if (lexema.matches("^[0-9]"))
-					precedencia = 4;
-				break;
-		}
-		return precedencia;
+		t1 = pila.peek();
+		pila.pop();
+		return t1;
 	}
 }
--- src/main/CodigoIntermedio.java
+++ src/main/CodigoIntermedio.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.Stack;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -22,13 +23,52 @@ public class CodigoIntermedio {
 	}
 
 	private void generarArboles() {
+		StringBuilder expresion = new StringBuilder();
 		Arbol abb = new Arbol();
+		boolean bandera = false;
 		ArrayList<Token> listaExpresiones = extraerExpresiones();
 		for (Token t : listaExpresiones) {
-			if (t.getExpresion().equals("E3"))
-				abb.insertar(t);
+			if (t.getExpresion().equals("E3") && bandera)
+				expresion.append(t.getSimbolo());
+			if (t.getExpresion().equals("E3") && t.getSimbolo().equals("=")) {
+				bandera = true;
+			}
 		}
-		abb.preOrden();
+		String cosa = convertirNotacion("" + expresion + ")");
+		System.out.println(cosa);
+		Nodo t = abb.construirArbol(cosa.toCharArray());
+		abb.infija(t);
+	}
+
+	private String convertirNotacion(String expresion) { //TODO: Ordenar esto.
+		String resultado = "";
+		Stack<Character> pila = new Stack<>();
+		pila.push('(');
+		for (int i = 0; i < expresion.length(); i++) {
+			char actual = expresion.charAt(i);
+			if (precedencia(actual) > 0) {
+				while (!pila.isEmpty() && precedencia(pila.peek()) >= precedencia(actual)) {
+					resultado += pila.pop();
+				}
+				pila.push(actual);
+			} else if (actual == ')') {
+				char auxiliar = pila.pop();
+				while (auxiliar != '(') {
+					resultado += auxiliar;
+					auxiliar = pila.pop();
+				}
+			} else if (actual == '(') {
+				pila.push(actual);
+			} else {
+				resultado += actual;
+			}
+		}
+		if (!pila.isEmpty()) {
+			for (int i = 0; i <= pila.size(); i++) {
+				resultado += pila.pop();
+			}
+		}
+		return resultado;
 	}
 
 	private ArrayList<Token> extraerExpresiones() {
@@ -39,5 +79,17 @@ public class CodigoIntermedio {
 
 	private void generarCuadruplos() {
 
+	}
+
+	private int precedencia(char c) {
+		switch (c) {
+			case '*':
+			case '/':
+				return 2;
+			case '+':
+			case '-':
+				return 1;
+		}
+		return -1;
 	}
 }
